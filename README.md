95d737fa4f7086bd4307b2628c07c561fa3e08c5
Xareth <sareth2277@gmail.com>
2022-08-28 20:41:14 +0200

    Add index.html; script.js & update README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rock-paper-scissors
+A Game of Rock-Paper-Scissors!
+
+For know it is to be only played in the console, further GUI implementations will follow.
---
+++ script.js
@@ -0,0 +1,14 @@
+
+// ==============================================================
+// Game of Rock-Paper-Scissors
+// ==============================================================
+
+// Following is needed:
+
+// - A way for user to choose his "sign"
+// - Automated Computer Response
+// - A function to compare the outputs
+// - Output the outcome
+
+// ==============================================================
+
