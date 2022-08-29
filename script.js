8ca14a5b79c64ffd00d95f2a4fe40ee894b92e91
Xareth <sareth2277@gmail.com>
2022-08-29 20:47:56 +0200

    Add functions for choices and for a single round

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-// ==============================================================
+// ==========================================================================
 // Game of Rock-Paper-Scissors
-// ==============================================================
+// ==========================================================================
 
 // Following is needed:
 
@@ -10,7 +10,14 @@
 // - A function to compare the outputs
 // - Output the outcome
 
-// ==============================================================
+// ==========================================================================
+
+
+function capitalizeStr (str) {
+    str =   str.charAt(0).toUpperCase() + 
+            str.slice(1).toLowerCase();
+    return str;
+}
 
 
 function getComputerChoice() {
@@ -20,7 +27,67 @@ function getComputerChoice() {
         "Paper", 
         "Scissors"
     ];
-
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+
+function getPlayerChoice() {
+    
+    const choice = capitalizeStr(prompt("Pick rock, paper or scissors: "));
+    
+    if (
+        choice !== "Rock" &&
+        choice !== "Paper" &&
+        choice !== "Scissors"
+    ) {
+        throw new Error ("Invalid choice! Please try again.");
+
+    } else {
+        return choice;
+    }
+}
+
+
+function singleRound(playerSelection, computerSelection) {
+    
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    
+
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock")
+    ) {
+        return `${playerSelection} beats ${computerSelection}! You've won!`;
+    
+    
+    // Scissors beat ... / not scissors beats ... //
+    } else if (
+        playerSelection === "Scissors" && computerSelection === "Paper"
+    ) {
+        return `${playerSelection} beat ${computerSelection}! You've won!`;
+
+    } else if (
+        playerSelection === "Paper" && computerSelection === "Scissors"
+    ) {
+        return `${computerSelection} beat ${playerSelection}! You've lost!`
+    } 
+    
+    
+    else {
+        return `${computerSelection} beats ${playerSelection}! You've lost!`
+    }
+}
+
+
+// ==========================================================================
+
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+
+console.log(singleRound(playerSelection, computerSelection));
